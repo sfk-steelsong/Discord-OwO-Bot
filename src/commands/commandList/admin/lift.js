29c8edbe35daed929e9920b65cc85aec431ce338
Alex Mudd <mudda09@gmail.com>
2020-07-18 16:45:37 -0500

    rework lift to not mess with penalty unless provided

--- src/commands/commandList/admin/lift.js
+++ src/commands/commandList/admin/lift.js
@@ -16,27 +16,39 @@ module.exports = new CommandInterface({
 	dm:true,
 
 	execute: async function(p){
-		let time;
+		let penalty;
 		if(p.global.isInt(p.args[1])){
-			time = parseInt(p.args[1]);
-		}else{
-			p.errorMsg(", Wrong time format");
-			return;
+			penalty = parseInt(p.args[1]);
 		}
 
 		if(!p.global.isUser("<@"+p.args[0]+">")){
 			p.errorMsg(", Invalid user id");
 			return;
 		}
-		let sql = "UPDATE IGNORE timeout SET penalty = "+time+" WHERE id = "+p.args[0]+";";
+		let sql = "UPDATE IGNORE timeout SET time = "+new Date(0).toISOString().split('T')[0]; // set ban time to epoch
+		if (penalty) {
+			sql += `, penalty = ${penalty}`;
+		}
+		sql += " WHERE id = "+p.args[0]+";"
 		let result = await p.query(sql);
 
-		if(user = await p.sender.msgUser(p.args[0],"**🙇 |** Your penalty has been lifted by an admin! Sorry for the inconvenience!"))
-			p.send("Penalty has been set to "+time+" for "+user.username);
-		else if(guild = await p.fetch.getGuild(p.args[0]))
-			p.send("Penalty has been set to "+time+" for guild: "+guild.name);
-		else
-			p.send("Failed to set penalty for that user");
+		if(user = await p.sender.msgUser(p.args[0],"**🙇 |** Your penalty has been lifted by an admin! Sorry for the inconvenience!")) {
+			let message = `Ban has been lifted for ${user.username}`;
+			if (penalty) {
+				message += `, penalty set to ${penalty} hours for future bans.`;
+			}
+			p.send(message);
+		}
+		else if(guild = await p.fetch.getGuild(p.args[0])) {
+			let message = `Ban has been lifted for guild: ${guild.name}`;
+			if (penalty) {
+				message += `, penalty set to ${penalty} hours for future bans.`;
+			}
+			p.send(message);
+		}
+		else {
+			p.send("If that was a user, they had DMs disabled; the ban was still lifted.  If it was a guild, the ban was not lifted.  Or it was neither and nothing happened.");
+		}
 	}
 
 })
